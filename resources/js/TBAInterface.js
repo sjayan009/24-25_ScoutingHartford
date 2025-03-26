5c75b5979033aae8832387e8651ec4fb053f010d
sjayan009 <70182014+sjayan009@users.noreply.github.com>
2025-03-26 16:05:30 -0400

    Update TBAInterface.js
    Updated Read API Key with Jayan's API Key

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "6PO9BY9wDFZ9vsX4YRq2uSnQrS0OV577EgBAzfOWiRhz7K6NdZuf6k0fToN2yb2z";
 /**
  * Get list of teams in event
  *
